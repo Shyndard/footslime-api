7b4c1cb742e2448a7abaf349997ca7ed4d717162
BuildTools <shyndard@gmail.com>
2020-05-28 02:28:33 +0200

    :bug: Fix cors configuration

--- src/main/java/com/shyndard/util/footslime/api/config/CorsConfiguration.java
+++ src/main/java/com/shyndard/util/footslime/api/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration {
 
-	@Value("${allowed-host}")
+	@Value("#{'${allowed-host}'.split(',')}")
 	private String[] allowedHosts;
 
 	@Bean
